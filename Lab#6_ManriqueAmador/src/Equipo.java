8c6053d89970d86442725976da756c4038238928
Tu nombre aquí“$ git config --global user.name Manrique“ <bmanrique17@unitec.edu>
2017-11-17 13:50:16 -0600

    Clases terminadas

---
+++ Lab#6_ManriqueAmador/src/Equipo.java
@@ -0,0 +1,69 @@
+
+import java.util.ArrayList;
+
+
+public class Equipo {
+    private String nombre,estadio;
+    private double presupuesto;
+    private int copas;
+    ArrayList<Jugador> listaJugadores = new ArrayList();;
+
+    public Equipo(String nombre, String estadio, double presupuesto, int copas) {
+        this.nombre = nombre;
+        this.estadio = estadio;
+        this.presupuesto = presupuesto;
+        this.copas = copas;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+    
+    
+    
+    public Equipo() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEstadio() {
+        return estadio;
+    }
+
+    public double getPresupuesto() {
+        return presupuesto;
+    }
+
+    public int getCopas() {
+        return copas;
+    }
+
+    public ArrayList<Jugador> getListaJugadores() {
+        return listaJugadores;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEstadio(String estadio) {
+        this.estadio = estadio;
+    }
+
+    public void setPresupuesto(double presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public void setCopas(int copas) {
+        this.copas = copas;
+    }
+
+    public void setListaJugadores(ArrayList<Jugador> listaJugadores) {
+        this.listaJugadores = listaJugadores;
+    }
+    
+    
+}
---
+++ Lab#6_ManriqueAmador/src/Jugador.java
@@ -0,0 +1,84 @@
+
+public class Jugador {
+    
+    private String nombre,posicion;
+    private double precio;
+    private boolean disponibilidad;
+    private int habilidad,tecnica,resistencia;
+
+    public Jugador(String nombre, String posicion, double precio, boolean disponibilidad, int habilidad, int tecnica, int resistencia) {
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.precio = precio;
+        this.disponibilidad = disponibilidad;
+        this.habilidad = habilidad;
+        this.tecnica = tecnica;
+        this.resistencia = resistencia;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " (" + posicion + ")";
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setDisponibilidad(boolean disponibilidad) {
+        this.disponibilidad = disponibilidad;
+    }
+
+    public void setHabilidad(int habilidad) {
+        this.habilidad = habilidad;
+    }
+
+    public void setTecnica(int tecnica) {
+        this.tecnica = tecnica;
+    }
+
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+    
+    public Jugador() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public boolean isDisponibilidad() {
+        return disponibilidad;
+    }
+
+    public int getHabilidad() {
+        return habilidad;
+    }
+
+    public int getTecnica() {
+        return tecnica;
+    }
+
+    public int getResistencia() {
+        return resistencia;
+    }
+    
+    
+}
